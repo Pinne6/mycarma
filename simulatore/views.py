6c38629a1de0c1c3abcb8066a8746e63d45f1492
Pinne <pinne@sadrock.it>
2016-10-18 14:01:57 +0200

    corretto settings

--- simulatore/views.py
+++ simulatore/views.py
@@ -106,7 +106,7 @@ def index(request):
                     tappeto[c].storico.append(Storico(data_ciclo))
                     data_ciclo += datetime.timedelta(days=1)
             for i in range(data_diff.days + 1):
-                if setting.SERVER_DEV is False:
+                if settings.SERVER_DEV is False:
                     filename = folder + crea_isin + "/" + data_inizio.strftime("%Y%m%d") + ".csv"
                 else:
                     filename = folder + crea_isin + "\\" + data_inizio.strftime("%Y%m%d") + ".csv"
