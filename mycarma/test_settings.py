1c24c76bb5beaa446c8003ecf244c2fc70439688
Pinne <pinne@sadrock.it>
2016-10-18 09:13:45 +0200

    show toolbar

--- mycarma/test_settings.py
+++ mycarma/test_settings.py
@@ -11,7 +11,5 @@ def show_toolbar(request):
     return True
 
 DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': 'YourAppName.settings.development.show_toolbar',
+    'SHOW_TOOLBAR_CALLBACK': show_toolbar,
 }
-
-INTERNAL_IPS = ['127.0.0.1']
